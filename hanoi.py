2909e85c5e77230756aca852f97d860e178b3c18
meghna125 <31645367+meghna125@users.noreply.github.com>
2017-09-05 11:59:40 +0530

    Add files via upload

---
+++ hanoi.py
@@ -0,0 +1,17 @@
+def hanoi(n,source,spare,target):
+    '''
+    objective : To build tower of hanoi using n number of disks and 3 poles 
+    input parameters :
+                 n -> no of disks
+                 source : starting position of disk
+                 spare : auxillary position of the disk
+                 target : end position of the disk
+    '''
+    #approach : call hanoi function recursively to move disk from one pole to another
+    assert n>0
+    if n==1:
+        print('Move disk from ',source,' to ',target)
+    else:
+        hanoi(n-1,source,target,spare)
+        print('Move disk from ',source,' to ',target)
+        hanoi(n-1,spare,source,target)
